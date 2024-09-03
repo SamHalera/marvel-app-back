9a4573cf4316571249783a4c7cdef37481784b71
SamHalera <samuel.halera@gmail.com>
2024-09-03 10:59:39 +0200

    dockerize for deploy

--- dist/utils/convertToBase64.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertToBase64 = void 0;
-const convertToBase64 = (file) => {
-    return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
-};
-exports.convertToBase64 = convertToBase64;
